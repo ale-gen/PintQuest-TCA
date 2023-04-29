61da77cf8c29d01a8f6ad6d2689e7922930fc248
Aleksandra Generowicz <o.generowicz@gmail.com>
2023-04-29 11:19:49 +0200

    Initial Commit

---
+++ PintQuest-TCA/PintQuest_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PintQuest_TCAApp.swift
+//  PintQuest-TCA
+//
+//  Created by Aleksandra Generowicz on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PintQuest_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
