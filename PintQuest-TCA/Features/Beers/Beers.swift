b0f0a4558e40215bac90483d30bc690f6ce4885d
Aleksandra Generowicz <o.generowicz@gmail.com>
2023-05-11 22:55:30 +0200

    Fix changing state for selected beer

--- PintQuest-TCA/Features/Beers/Beers.swift
+++ PintQuest-TCA/Features/Beers/Beers.swift
@@ -48,66 +48,72 @@ struct Beers: ReducerProtocol {
     
     private enum BeerCompletionID {}
     
-    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-        switch action {
-        case .onAppear:
-            guard state.beers.isEmpty else { return .init(value: .retrieveFavorites) }
-            return .init(value: .retrieve)
-            
-        case .retrieve:
-            state.beers = []
-            state.currentPage = 1
-            return .task { [page = state.currentPage] in
-                await .beersResponse(TaskResult { try await self.punkAPIClient.fetchByPage(page) })
-            }
-            //            return .concatenate(
-            //                .init(value: .loadingActive(true)),
-            //                .init(value: )
-            //            )
-            
-        case .retrieveNextPageIfNeeded(currentItemId: let itemId):
-            guard state.isLastItem(itemId) else { return .none }
-            state.currentPage += 1
-            return .task { [page = state.currentPage] in
-                await .beersResponse(TaskResult { try await self.punkAPIClient.fetchByPage(page) })
-            }
-            
-        case .retrieveFavorites:
-            // TODO: Fetch fav beers from fav client
-            return .none
-            
-        case .beersResponse(.success(let beers)):
-            beers.forEach { beer in
-                state.beers.append(
-                    BeerDetails.State(id: UUID(),
-                                      beer: beer)
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                guard state.beers.isEmpty else { return .init(value: .retrieveFavorites) }
+                return .init(value: .retrieve)
+                
+            case .retrieve:
+                state.beers = []
+                state.currentPage = 1
+                return .task { [page = state.currentPage] in
+                    await .beersResponse(TaskResult { try await self.punkAPIClient.fetchByPage(page) })
+                }
+                //            return .concatenate(
+                //                .init(value: .loadingActive(true)),
+                //                .init(value: )
+                //            )
+                
+            case .retrieveNextPageIfNeeded(currentItemId: let itemId):
+                guard state.isLastItem(itemId) else { return .none }
+                state.currentPage += 1
+                return .task { [page = state.currentPage] in
+                    await .beersResponse(TaskResult { try await self.punkAPIClient.fetchByPage(page) })
+                }
+                
+            case .retrieveFavorites:
+                // TODO: Fetch fav beers from fav client
+                return .none
+                
+            case .beersResponse(.success(let beers)):
+                beers.forEach { beer in
+                    state.beers.append(
+                        BeerDetails.State(id: UUID(),
+                                          beer: beer)
+                    )
+                }
+                return .concatenate(
+                    .init(value: .loadingActive(false)),
+                    .init(value: .loadingPageActive(false))
                 )
+                
+            case .beersResponse(.failure(_)):
+                return .concatenate(
+                    .init(value: .loadingActive(false)),
+                    .init(value: .loadingPageActive(false))
+                )
+                
+            case .favoritesResponse(.success(let favBeers)):
+                state.favourites = favBeers
+                return .none
+                
+            case .loadingActive(let isLoading):
+                state.isLoading = isLoading
+                return .none
+                
+            case .loadingPageActive(let isLoadingPage):
+                state.isLoadingPage = isLoadingPage
+                return .none
+                
+            default:
+                return .none
             }
-            return .concatenate(
-                .init(value: .loadingActive(false)),
-                .init(value: .loadingPageActive(false))
-            )
-            
-        case .beersResponse(.failure(_)):
-            return .concatenate(
-                .init(value: .loadingActive(false)),
-                .init(value: .loadingPageActive(false))
-            )
-            
-        case .favoritesResponse(.success(let favBeers)):
-            state.favourites = favBeers
-            return .none
-            
-        case .loadingActive(let isLoading):
-            state.isLoading = isLoading
-            return .none
-            
-        case .loadingPageActive(let isLoadingPage):
-            state.isLoadingPage = isLoadingPage
-            return .none
-            
-        default:
-            return .none
+        }
+        
+        .forEach(\.beers, action: /Action.beer(id:action:)) {
+            BeerDetails()
         }
     }
 }
